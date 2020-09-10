a79648bd3dfcca1e6483b17a8bcd5192fba3d7a0
staradan <44818251+staradan@users.noreply.github.com>
2020-09-10 15:03:03 -0500

    built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "typeface-roboto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App App-header">
+      <h1>Hi! My name is <span className="red">Daniel Stara.</span></h1>
+      <p>
+        I am a developer <span className="red">(love UI/UX)</span>, college student,
+        bullpen catcher, and avid podcast consumer. Here are some of my projects.
+      </p>
+      <div className="flex-container">
+        <a className="inline" href="https://statpracticetwo.web.app">
+          <h4>UNL Baseball Practice Stats</h4>
         </a>
-      </header>
+        <a className="inline" href="https://statpracticetwo.web.app">
+          <h4>David City Barber Shop</h4>
+        </a>
+        <a className="inline" href="https://statpracticetwo.web.app">
+          <h4>UNL Food Delivery</h4>
+        </a>
+      </div>
     </div>
   );
 }
