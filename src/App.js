8eaba65c990110873af694b086115da339781ef6
staradan <dstara12@gmail.com>
2022-09-20 11:28:15 -0500

    updated it

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App App-header">
-      <h1>Hi! My name is <span className="red">Daniel Stara.</span></h1>
+      <h1>Hi! My name is Daniel Stara</h1>
       <p>
-        I am a developer <span className="red">(love UI/UX)</span>, college student,
-        bullpen catcher, and avid podcast consumer. Here are some of my projects.
+        I am a full-stack developer fired up by innovative design and rapid product development.
+      </p>
+      <p>
+        In my spare time I'm a college student,
+        bullpen catcher, and pickup basketball MVP. Here are some of my projects.
       </p>
       <div className="flex-container">
-        <a className="inline" href="https://statpracticetwo.web.app">
-          <h4>UNL Baseball Practice Stats</h4>
+        <a className="inline" href="https://tappsports.com">
+          <h4>Tapp sports</h4>
         </a>
-        <a className="inline" href="https://davidcitybarber.com">
-          <h4>David City Barber Shop</h4>
+        <a className="inline" href="https://pitchx.netlify.com">
+          <h4>Husker baseball pitch by pitch</h4>
         </a>
         <a className="inline" href="https://unlfd.netlify.com">
           <h4>UNL Food Delivery</h4>
